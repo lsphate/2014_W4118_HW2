08a6d1a40258e962403c07671df2b2207fffa75d
W4118 Student <w4118@w4118>
2014-09-21 18:20:05 -0400

    Some modify for add syscall

---
+++ kernel/arch/arm/kernel/ptree.c
@@ -0,0 +1,9 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+
+asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
+{
+	printk("Congrats, your new system call has been called successfully");
+	return 0;
+}
