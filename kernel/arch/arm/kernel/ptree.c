70e22791532bc6aefea2b1f0721f4bcf13b8d4ba
W4118 Student <w4118@w4118>
2014-09-22 16:04:13 -0400

    ptree test

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -2,9 +2,18 @@
 #include <linux/kernel.h>
 #include <linux/prinfo.h>
 #include <linux/syscalls.h>
+#include <linux/sched.h>
+#include <linux/list.h>
 
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 {
+	read_lock(&tasklist_lock); 
+	
+	struct task_struct *p = &init_task;
+	
+	printk("%s", p->comm);
+
+		read_unlock(&tasklist_lock);
 	printk("Congrats, your new system call has been called successfully");
 	return 0;
 }
