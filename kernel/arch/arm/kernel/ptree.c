5b34799c7f0ea16d3f55d3e49d11d913de163a95
W4118 Student <w4118@w4118>
2014-09-22 16:53:55 -0400

    found init process task_struct

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -10,8 +10,11 @@ asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 	read_lock(&tasklist_lock); 
 	
 	struct task_struct *p = &init_task;
-	
-	printk("%s", p->comm);
+
+	printk("PARENT PROCESS: ");
+	struct task_struct *initTask;
+	initTask = list_entry(p->children.next, struct task_struct, sibling);
+	printk("%s\n", initTask->comm);
 
 		read_unlock(&tasklist_lock);
 	printk("Congrats, your new system call has been called successfully");
