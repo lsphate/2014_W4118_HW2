f698ad521397cf5f76d81312eb95fc46aeadb884
W4118 Student <w4118@w4118>
2014-09-23 10:41:47 -0400

    traverse task list and pring - success

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -11,12 +11,18 @@ asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 	
 	struct task_struct *p = &init_task;
 
-	printk("PARENT PROCESS: ");
+	/*printk("PARENT PROCESS: ");
 	struct task_struct *initTask;
 	initTask = list_entry(p->children.next, struct task_struct, sibling);
 	printk("%s\n", initTask->comm);
-
-		read_unlock(&tasklist_lock);
+	*/
+	struct list_head *i;
+	list_for_each(i, &p->children) {
+		struct task_struct *currentTask;
+		currentTask = list_entry(i, struct task_struct, sibling);
+		printk("%s\n", currentTask->comm);
+	}
+	read_unlock(&tasklist_lock);
 	printk("Congrats, your new system call has been called successfully");
 	return 0;
 }
