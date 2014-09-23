4a0db8dc7b82d4737a77c0afd14efb8de0414a27
W4118 Student <w4118@w4118>
2014-09-23 16:01:46 -0400

    prinfo.c with tabs completed

--- prinfo.c
+++ prinfo.c
@@ -1,10 +1,68 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+#include "prinfo.h"
+
 int main(int argc, char **argv) {
+	
 	int x;
-	x = syscall(223);
-	if (x == 0) printf("success\n");
-	else perror("error");
+	int nr =10 ;
+	int layer ;
+	
+	struct prinfo  *buff[ nr ];
+
+	x = syscall(223, buff, nr);
+
+	if (x == 0) 
+		printf("success\n");
+	else 
+		perror("error");
+
+	nr = x ;
+	struct prinfo *buf[ nr ];
+	x = syscall( 223, buf, nr);
+	
+
+
+/*
+
+
+
+
+
+DFS here
+
+
+
+
+
+*/
+
+
+	int i ;
+
+	while (buf[i]!=NULL) {
+		if ( buf[i] -> parent_pid == 0 )
+                        layer = 0 ;	
+		int j ;
+		for ( j = 0 ; j < layer ; j++) {
+       			printf("        ");
+		}
+       			printf("%s,%d,%ld,%d,%d,%d,%ld\n", buf[i]->comm, buf[i]->pid, buf[i]->state, buf[i]->parent_pid, buf[i]->first_child_pid,
+				buf[i]->next_sibling_pid , buf[i]->uid);
+
+		if ( buf[i] -> next_sibling_pid == 0) {
+			if( buf[i]->first_child_pid !=0 )
+				layer ++ ;
+			else 
+				layer -- ;
+		}
+		else if ( buf[i]->first_child_pid != 0 ) {
+			layer ++ ;
+			
+		}
+	i++;
+	}	
+
 	return 0;
 }
