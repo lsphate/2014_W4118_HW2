88fe9f249c4c11f8210afc903e1feb48d1c63347
Angel Say <aas2215@columbia.edu>
2014-09-25 01:10:07 -0400

    more progress on dfs stack

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -43,6 +43,18 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 		prinlist[count].original_task = i;
 		INIT_LIST_HEAD(&(prinlist[count].dfs_order));
 		list_add(&(prinlist[count].dfs_order), &(dfsStack.dfs_order));
+		/* As long as there are children in the stack visit */
+		struct list_head *top;
+
+		top = &(dfsStack.dfs_order);
+		while(top->next != &dfsStack.dfs_order) {
+			struct prinfo_list *currentTask;
+			currentTask = list_entry(top->next, struct prinfo_list, dfs_order);
+			list_del(top->next);
+			struct task_struct *y;
+			y = list_entry(currentTask->original_task, struct task_struct, sibling);
+			printk("%s,%d,%ld,%d\n", y->comm, y->pid, y->state, y->parent->pid);
+		}
 	/*	
 		struct task_struct *x;
 		struct prinfo_list *y;
@@ -62,7 +74,7 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 	*/	
 		count++;	
 	}
-
+/*
 	list_for_each(i, prinlist->dfs_order.next) {
 		struct prinfo_list *x;
 		struct task_struct *y;
@@ -70,7 +82,7 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 		y = list_entry(x->original_task, struct task_struct, sibling);
 		printk("%s,%d,%ld,%d\n", y->comm, y->pid, y->state, y->parent->pid);
 	}
-		
+*/		
 	/////////////////////////////////////DFS HERE !!!!!!!!!! ////////////////////////////////////
 /*	
 		index = 1;
