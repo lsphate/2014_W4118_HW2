9d2df6f9174b26afd075aca590e14988388bc172
W4118 Student <w4118@w4118>
2014-09-24 21:53:36 -0400

    DFS v.1

--- kernel/arch/arm/kernel/ptree.c
+++ kernel/arch/arm/kernel/ptree.c
@@ -10,7 +10,7 @@
 SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 {
 	struct prinfo * prinfoBuf;
-	int bufSize;
+	int bufSize, procnum, index;
 
 	if (copy_from_user(&bufSize, nr, sizeof(int)))
 		return -EFAULT;
@@ -26,6 +26,14 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 	struct task_struct *p = &init_task;
 	struct list_head *i;
 	
+	
+	for_each_process(p) {
+		procnum++;	
+	} 
+
+	struct prinfo_list prinlist[procnum];
+	p = init_task;
+
 	for_each_process(p) {
 		struct task_struct *x;
 		pid_t first_childPID = 0;
@@ -41,7 +49,56 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 		
 		x = list_entry(p->sibling.next, struct task_struct, sibling);
 		printk("%s,%d,%ld,%d,%d,%d\n", p->comm, p->pid, p->state, p->parent->pid, first_childPID, next_siblingPID);
+		
+		
+		prinlist[index].prinfo.parent_pid = p->parent->pid;
+		prinlist[index].prinfo.pid = p->pid;
+		prinlist[index].prinfo.first_child_pid = first_childPID;
+		prinlist[index].prinfo.next_sibling_pid = next_siblingPID;
+		prinlist[index].prinfo.state = p->state;
+		prinlist[index].prinfo.uid = p->uid;
+		prinlist[index].prinfo.comm = p->comm;
+		
+		
+	//	prinlist[index].original_task = p->sibling;	
+		index++;
 	}
+		
+	/////////////////////////////////////DFS HERE !!!!!!!!!! ////////////////////////////////////
+	
+		index = 1;
+		int i;
+		struct prinfo_list temp ;
+
+		for (index ; index < procnum-1 ; index ++ ) {
+			if (prinlist[index-1].prinfo.next_child_pid!=0)
+				for (i = index ; i < procnum ; i++ ) {
+					if (prinlist[i].prinfo.pid == prinlist[index-1].prinfo.next_child_pid )
+						temp = prinlist[i];
+						prinlist[i] = prinlist[index];
+						prinlist[index] = temp;
+				}	
+			else if (prinlist[index-1].prinfo.next_sibling_pid!=0)
+				for (i = index ; i < procnum ; i++ ) {
+                                	if (prinlist[i].prinfo.pid == prinlist[index-1].prinfo.next_sibling_pid )
+                                                temp = prinlist[i];
+                                                prinlist[i] = prinlist[index];
+                                                prinlist[index] = temp;
+                                }
+			else {
+				temp = prinlist[index] ;
+				for (i = index ; i < procnum ; i++) {	
+					if (prinlist[index].prinfo.pid < temp)
+						temp = prinlist[ index ];
+				}
+				prinlist[i] = prinlist[index];
+				prinlist[index] = temp; 
+			}
+		}
+
+
+
+
 	read_unlock(&tasklist_lock);
 	printk("Congrats, your new system call has been called successfully");
 	return 0;
