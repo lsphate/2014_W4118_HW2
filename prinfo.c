230862a7603c41d053e63bcc74b7f785b3767a7b
sl3833@columbia.edu <w4118@w4118>
2014-09-25 20:13:17 -0400

    checkpatch

--- prinfo.c
+++ prinfo.c
@@ -6,38 +6,42 @@
 #include <errno.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int x;
 	int nr = 70;
 	int layer =  1;
 	struct prinfo buf[nr];
 
 	x = syscall(223, buf, &nr);
-	if (x != 0) 
+	if (x != 0)
 		printf("error: %s\n", strerror(errno));
 
 	int i = 0;
 
-	printf("%s,%d,%ld,%d,%d,%d,%ld\n", buf[i].comm, buf[i].pid, buf[i].state,
-                                buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
+	printf("%s,%d,%ld,%d,%d,%d,%ld\n",
+buf[i].comm, buf[i].pid, buf[i].state,
+buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
+
 	i++;
 	while (i < nr && buf[i].pid != 0) {
 		if (buf[i].parent_pid == 0)
-                        layer = 1;	
+			layer = 1;
 		int j;
-		for (j = 0 ; j < layer ; j++) {
-       			printf("\t");
-		}
-		printf("%s,%d,%ld,%d,%d,%d,%ld\n", buf[i].comm, buf[i].pid, buf[i].state,
-				buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
+
+		for (j = 0 ; j < layer ; j++)
+			printf("\t");
+
+		printf("%s,%d,%ld,%d,%d,%d,%ld\n",
+buf[i].comm, buf[i].pid, buf[i].state,
+buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
 
 		if (buf[i].next_sibling_pid == 0) {
-			if(buf[i].first_child_pid != 0)
+			if (buf[i].first_child_pid != 0)
 				layer++;
-			else 
+			else
 				layer--;
-		}
-		else if (buf[i].first_child_pid != 0) {
+		} else if (buf[i].first_child_pid != 0) {
 			layer++;
 		}
 	i++;
